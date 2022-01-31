1a4cd646267cb78bfcffa21e4c03f9bd344490af
Filip Havlíček <filip.havlicek@livesport.eu>
2022-01-31 14:00:10 +0100

    server.js - print env variable KUBERNETES_NODE_NAME (e.g. set variable with Downward API in pod's definition)

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var handleRequest = function(request, response) {
   console.log('Headers: ' + JSON.stringify(request.headers));
   response.writeHead(200);
   response.write('Hostname: ' + require('os').hostname() + '\n' + 'Timestamp: ' + new Date().getTime() + '\n');
+  if (typeof process.env.KUBERNETES_NODE_NAME !== 'undefined') {
+    response.write(process.env.KUBERNETES_NODE_NAME + '\n');
+  };
   response.end();
 };
 var www = http.createServer(handleRequest);
